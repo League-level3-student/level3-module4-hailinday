4c0260efb409fef7022a62a214e85047ca14f328
hailinday <54258211+hailinday@users.noreply.github.com>
2022-03-26 21:52:51 -0700

    starting

--- src/_00_IntroToStacks/_01_IntroToStack.java
+++ src/_00_IntroToStacks/_01_IntroToStack.java
@@ -2,16 +2,17 @@ package _00_IntroToStacks;
 
 import java.util.Random;
 import java.util.Stack;
-
 import javax.swing.JOptionPane;
-
 public class _01_IntroToStack {
     public static void main(String[] args) {
         // 1. Create a Stack of Doubles
         //    Don't forget to import the Stack class
-
+    	Stack<Double> dob = new Stack<Double>();
         // 2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-
+    	while (dob.size()<100) {
+			Random rand = new Random();
+    		dob.push(rand.next);
+		}
         // 3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
 
         // 4. Pop all the elements off of the Stack. Every time a double is popped that is

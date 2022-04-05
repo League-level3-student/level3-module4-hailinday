9fa16ac0c2cc092f22d9ef3b781759b6f954b5a3
hailinday <54258211+hailinday@users.noreply.github.com>
2022-04-04 19:52:54 -0700

    stuck

--- src/_00_IntroToStacks/_01_IntroToStack.java
+++ src/_00_IntroToStacks/_01_IntroToStack.java
@@ -11,18 +11,24 @@ public class _01_IntroToStack {
         // 2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
     	while (dob.size()<100) {
 			Random rand = new Random();
-    		dob.push(rand.next);
+    		dob.push((double) rand.nextInt(100));
 		}
         // 3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-
+    	String num1 = JOptionPane.showInputDialog("Enter a number between 1 and 100");
+    	String num2 = JOptionPane.showInputDialog("Enter another number between 1 and 100");
         // 4. Pop all the elements off of the Stack. Every time a double is popped that is
         //    between the two numbers entered by the user, print it to the screen.
-
-
+    	Double x = Double.parseDouble(num1);
+    	Double y = Double.parseDouble(num2);
         // EXAMPLE:
         // NUM 1: 65
         // NUM 2: 75
-
+    	System.out.println("Elements between " + x + " and " + y);
+    	while( ! dob.isEmpty() ) {
+    		if (dob.pop()>=x && dob.pop()<=y) {
+            	System.out.println(dob.pop());
+			}
+        }
         // Popping elements off stack...
         // Elements between 65 and 75:
         // 66.66876846

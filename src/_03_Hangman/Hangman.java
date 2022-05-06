ee9ce2f2d50db1be04e4dd91c14aa8a1521cddf6
jeremycui2008 <jeremycuid@gmail.com>
2022-05-06 16:57:51 -0700

    Hangman done. maze begins

--- src/_03_Hangman/Hangman.java
+++ src/_03_Hangman/Hangman.java
@@ -11,21 +11,24 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Hangman implements KeyListener {
+	Stack<String> hang = new Stack<String>();
+	int lives = 10;
+	StringBuilder builder = new StringBuilder("");
+	String hangWord = "";
+	JFrame frame = new JFrame();
+    JPanel panel = new JPanel();
+    JLabel label = new JLabel();
 	public static void main(String[] args) {
         Hangman man = new Hangman();
         man.setup();
 	}
 	public void setup() {
-		JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
-        JLabel label = new JLabel();
         frame.add(panel);
         panel.add(label);
-        label.addKeyListener(this);
+        frame.addKeyListener(this);
         frame.setVisible(true);
         frame.pack();
-        
-		Stack<String> hang = new Stack<String>();
+   
 		String num = JOptionPane.showInputDialog("Enter a number from 1 to 266");
 		int amm = Integer.parseInt(num);
 		for (int i = 0; i < amm; i++) {
@@ -36,17 +39,47 @@ public class Hangman implements KeyListener {
 				hang.add(push);
 			}
 		}
-		String hangWord = hang.pop();
-		String text = "";
+		newWord();
+	}
+	public void newWord() {
+		hangWord = hang.pop();
+		System.out.println(hangWord);
 		for (int i = 0; i < hangWord.length(); i++) {
-			text = text + "_";
-			label.setText(text);
+			builder.append("_");
+			label.setText(builder.toString());
+			frame.pack();
 		}
 	}
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+		int lifeCheck = 0;
+		int popCheck = 0;
+		for (int i = 0; i < builder.length(); i++) {
+			if (hangWord.charAt(i)==e.getKeyChar()) {
+				lifeCheck+=1;
+				String rep = "" + hangWord.charAt(i);
+				label.setText(builder.replace(i, i+1, rep).toString());
+				frame.pack();
+			}
+		}
+		for (int i = 0; i < hangWord.length(); i++) {
+			String check = "" + builder.toString().charAt(i);
+			if (check.equalsIgnoreCase("_")) {
+				popCheck+=1;
+			}
+		}
+		if (popCheck==0) {
+			lives = 10;
+			builder.delete(0, builder.length());
+			newWord();
+		}
+		if (lifeCheck==0) {
+			lives-=1;
+		}
+		if (lives==0) {
+			JOptionPane.showMessageDialog(null, "You lost but you can continue if you'd like to!");
+		}
 	}
 	@Override
 	public void keyPressed(KeyEvent e) {
--- src/_04_Maze_Maker/Maze.java
+++ src/_04_Maze_Maker/Maze.java
@@ -1,9 +1,10 @@
 package _04_Maze_Maker;
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 public class Maze {
     // 1. Create a 2D array of cells. Don't initialize it.
-    
+    Cell[][] cList;
     private int rows;
     private int cols;
 
@@ -12,20 +13,28 @@ public class Maze {
         this.cols = cols;
 
         // 2. Initialize the cells array using the rows and cols variables
-
+        cList = new Cell[rows][cols];
         // 3. Iterate through each cell and initialize it
         //    using row and col as the cell location
-
+        for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				cList[i][j]= new Cell(i,j);
+			}
+		}
     }
 
     // 4. This method iterates through the cells and draws them
     public void draw(Graphics g) {
-
+    	for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				cList[i][j].draw(g);
+			}
+    	}
     }
 
     // 5. This method returns the selected cell
     public Cell getCell(int row, int col){
-        return  null;
+        return  cList[row][col];
     }
 
     public int getRows() {
